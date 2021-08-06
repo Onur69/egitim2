2061b37c3d2661e0011316fc86c4cc8f99ca5eef
Onur69 <69090848+Onur69@users.noreply.github.com>
2021-08-06 15:11:30 +0300

    first

---
+++ assets/js/my.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    mouseDrag:true,
+    smartSpeed:1000,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+    },
+});
